fa821403397f89ba9da627912055556ea30af540
Gordon Conness <gordon.conness1@gmail.com>
2020-05-09 09:36:52 -0500

    finishing up some things

--- FriendFinder/app/data/friends.js
+++ FriendFinder/app/data/friends.js
@@ -2,27 +2,27 @@
 	{
 		name: "",
 		photo: "",
-		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+		scores: []
 	},
 	{
 		name: "",
 		photo: "",
-		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+		scores: []
 	},
 	{
 		name: "",
 		photo: "",
-		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+		scores: []
 	},
 	{
 		name: "",
 		photo: "",
-		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+		scores: []
 	},
 	{
         name: "",
         photo: "",
-		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+		scores: []
 	}
 ];
 
