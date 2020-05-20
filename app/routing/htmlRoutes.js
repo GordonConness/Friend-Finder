388d6ec002a002f28111ce89170df62c4536ac92
Gordon Conness <gordon.conness1@gmail.com>
2020-05-20 11:31:26 -0500

    moving stuff more

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -21,7 +21,7 @@ module.exports = function(app) {
 
   // If no matching route is found default to home
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./index.html"));
+    res.sendFile(path.join(__dirname,"/../public/index.html" ));
   });
 
 };
